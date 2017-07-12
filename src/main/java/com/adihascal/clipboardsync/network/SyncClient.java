e65905bc4ea34d777db733ce06a6b963128ae798
Adi Hascal <adi.hascal@gmail.com>
2017-07-12 19:13:13 +0300

    reverted packet size increase. turned out to be meaningless

--- src/main/java/com/adihascal/clipboardsync/network/SyncClient.java
+++ src/main/java/com/adihascal/clipboardsync/network/SyncClient.java
@@ -28,7 +28,6 @@ public class SyncClient extends Thread
 			if(flavor != null)
 			{
 				Socket s = new Socket(phoneAddress, Main.getPort());
-				s.setReceiveBufferSize(1048576);
 				ClipHandlerRegistry.getHandlerFor(flavor.getMimeType()).sendClip(s, this.object);
 				s.close();
 				System.out.println("data sent");
